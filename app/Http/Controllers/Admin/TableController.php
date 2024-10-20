3e4c9cf3e3f3c659e739891b97c2a05068025a5d
filipkoudelnicek@seznam.cz <62012471+filipkoudelnicek@users.noreply.github.com>
2024-10-20 22:53:49 +0200

    Table, Reservation, validation
    Edit, delete, store Table nad Reservation, Date validation, display errors

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TableStoreRequest;
 use Illuminate\Http\Request;
 use App\Models\Table;
 
@@ -28,9 +29,16 @@ class TableController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TableStoreRequest $request)
     {
-        //
+        Table::create([
+            'name' => $request->name,
+            'number_of_guests' => $request->number_of_guests,
+            'status' => $request->status,
+            'location' => $request->location,
+        ]);
+
+        return to_route('admin.tables.index');
     }
 
     /**
@@ -44,24 +52,28 @@ class TableController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Table $table)
     {
-        //
+        return view('admin.tables.edit', compact('table'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(TableStoreRequest $request, Table $table)
     {
-        //
+        $table->update($request->validated());
+
+        return to_route('admin.tables.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Table $table)
     {
-        //
+         $table->delete();
+
+         return to_route('admin.tables.index');
     }
 }
